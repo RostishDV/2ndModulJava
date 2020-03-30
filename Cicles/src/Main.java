141d6a17cff3f55eec1c6cee6e92865d915aead7
Rostislav <altair15042001@mail.ru>
2020-03-30 21:17:22 +0300

    4th task added

---
+++ Cicles/src/Main.java
@@ -0,0 +1,31 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        //for
+        for (int i = 200000; i <= 210000 ; i ++)
+        {
+            System.out.println("Ticket number is: " + i);
+        }
+        for (int i = 220000; i <= 235000 ; i ++)
+        {
+            System.out.println("Ticket number is: " + i);
+        }
+
+        //============================================================
+        //while
+        int i = 200000;
+        while (i <= 210000)
+        {
+            System.out.println("Ticket number is: " + i);
+            i++;
+        }
+        i = 220000;
+        while (i <= 235000)
+        {
+            System.out.println("Ticket number is: " + i);
+            i++;
+        }
+
+    }
+}
