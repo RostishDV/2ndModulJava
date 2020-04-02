a15c3709a8c65fc7628582f836593f31564e9f84
Rostislav <altair15042001@mail.ru>
2020-04-02 18:51:05 +0300

    Cycles rework

--- Cicles/src/Main.java
+++ Cicles/src/Main.java
@@ -3,13 +3,12 @@ public class Main
     public static void main(String[] args)
     {
         //for
-        for (int i = 200000; i <= 210000 ; i ++)
+        for (int i = 200000; i <= 235000 ; i ++)
         {
-            System.out.println("Ticket number is: " + i);
-        }
-        for (int i = 220000; i <= 235000 ; i ++)
-        {
-            System.out.println("Ticket number is: " + i);
+            if (i <= 210000 || i >= 220000)
+            {
+                System.out.println("Ticket number is: " + i);
+            }
         }
     }
 }
